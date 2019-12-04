9af7863e48eafe2fee1a20cadcf36e846016d6b1
Isaac Batista <isaacbatst@gmail.com>
2019-12-04 00:00:04 -0300

    top view init

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 40 }}>
+      <View style={styles.topView}>
+        <TextInput placeholder="Course Goal" style={styles.goalsInput} />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  goalsInput: { width: 200, padding: 10, borderColor: "black", borderWidth: 1 },
+  topView: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  }
 });

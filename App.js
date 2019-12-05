4b7aa6d84c5d271b2667d41a08f9df3ce2103546
Isaac Batista <isaacbatst@gmail.com>
2019-12-04 21:59:14 -0300

    initial boxes layout

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={{ padding: 40 }}>
-      <View style={styles.topView}>
-        <TextInput placeholder="Course Goal" style={styles.goalsInput} />
-        <Button title="ADD" />
+      <View style={styles.firstBox}>
+        <Text>1</Text>
       </View>
-      <View></View>
+      <View style={styles.secondBox}>
+        <Text>2</Text>
+      </View>
+      <View style={styles.thirdBox}>
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  goalsInput: { width: 200, padding: 10, borderColor: "black", borderWidth: 1 },
-  topView: {
-    flexDirection: "row",
-    justifyContent: "space-around",
+  firstBox: {
+    backgroundColor: "red",
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  secondBox: {
+    backgroundColor: "blue",
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  thirdBox: {
+    backgroundColor: "green",
+    width: 100,
+    height: 100,
+    justifyContent: "center",
     alignItems: "center"
   }
 });

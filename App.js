504f81d60354a6980d7e4818cc5fcc568c1adecc
Isaac Batista <isaacbatst@gmail.com>
2019-12-04 23:14:36 -0300

    flatlist

--- App.js
+++ App.js
@@ -1,23 +1,65 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Text,
+  ScrollView,
+  FlatList
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const handleChangeText = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const handleAddGoal = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
   return (
-    <View style={{ padding: 40 }}>
-      <View style={styles.topView}>
-        <TextInput placeholder="Course Goal" style={styles.goalsInput} />
-        <Button title="ADD" />
+    <View style={styles.screen}>
+      <View style={styles.formView}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.goalsInput}
+          onChangeText={handleChangeText}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={handleAddGoal} />
       </View>
-      <View></View>
+      <FlatList
+        keyExtractor={(item,index) => index.toString()}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
   goalsInput: { width: 200, padding: 10, borderColor: "black", borderWidth: 1 },
-  topView: {
+  formView: {
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     alignItems: "center"
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1
   }
 });
